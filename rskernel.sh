25b2482b5d658a02612ecbd0d4877d1be8648876
uxh <522129870@qq.com>
2020-05-10 14:10:41 +0800

    add virt check

--- rskernel.sh
+++ rskernel.sh
@@ -17,101 +17,132 @@ plain='\033[0m'
 #Check root
 [[ $EUID -ne 0 ]] && echo -e "${red}This script must be run as root!${plain}" && exit 1
 
-#Start information
-clear
-echo "#====================================================================#"
-echo "# One-click Change Kernel to Adapt Serverspeeder for CentOS 6/7 v2.1 #"
-echo "# Github: https://github.com/uxh/awesome-linux-tools                 #"
-echo "# Author: www.banwagongzw.com && www.vultrcn.com                     #"
-echo "#====================================================================#"
-echo ""
-
 #Check system
 function check_release(){
     local value=$1
+    local release="none"
 
-    local release=''
-
-    if [[ -f /etc/redhat-release ]]; then
+    if [ -f /etc/redhat-release ]; then
         release="centos"
-    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    elif grep -qi "centos|red hat|redhat" /etc/issue; then
         release="centos"
-    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    elif grep -qi "centos|red hat|redhat" /proc/version; then
+        release="centos"
+    elif grep -qi "centos|red hat|redhat" /etc/*-release; then
         release="centos"
     fi
 
-    if [ "$value" == "$release" ]; then
+    if [[ ${value} == ${release} ]]; then
         return 0
     else
         return 1
     fi
 }
 
-#Get centos main version
-function get_centos_main_version(){
-    if [[ -s /etc/redhat-release ]]; then
-        grep -oE  "[0-9.]+" /etc/redhat-release
+#Check virt
+function check_virt(){
+    yum install -y virt-what
+
+    local value=$1
+    local virt=$(virt-what)
+
+    if [ "${value}" == "${virt}" ]; then
+        return 0
     else
-        grep -oE  "[0-9.]+" /etc/issue
+        return 1
     fi
 }
 
 #Check centos main version
-function check_centos_main_version(){
-    local num=$1
+function check_centos_main_version() {
+    local value=$1
+    local version="0.0.0"
 
-    local version="$(get_centos_main_version)"
-    local main_ver=${version%%.*}
+    if [ -s /etc/redhat-release ]; then
+        version=$(grep -Eo "[0-9.]+" /etc/redhat-release)
+    else
+        version=$(grep -Eo "[0-9.]+" /etc/issue)
+    fi
 
-    if [ "$num" == "$main_ver" ]; then
+    local mainversion=${version%%.*}
+
+    if [ ${value} -eq ${mainversion} ]; then
         return 0
     else
         return 1
     fi
 }
 
+#Start information
+function start(){
+    clear
+    echo "#====================================================================#"
+    echo "# One-click Change Kernel to Adapt Serverspeeder for CentOS 6/7 v2.2 #"
+    echo "# Github: https://github.com/uxh/awesome-linux-tools                 #"
+    echo "# Author: www.banwagongzw.com && www.vultrcn.com                     #"
+    echo "#====================================================================#"
+    echo ""
+    echo "Press Enter to continue...or Press Ctrl+C to cancel"
+    read -n 1
+}
+
 #Main
+if [ -s /etc/selinux/config ] && grep "SELINUX=enforcing" /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
 if check_release centos; then
-    if check_centos_main_version 6; then
-        echo -e "[${green}INFO${plain}] System OS is CentOS6. Processing..."
-        echo -e "-------------------------------------------"
-        rpm -ivh https://filedown.me/Linux/Kernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
-        rpm -ivh https://filedown.me/Linux/Kernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
-        if [ $? -eq 0 ]; then
-            number=$(cat /boot/grub/grub.conf | awk '$1=="title" {print i++ " : " $NF}' | grep '2.6.32-504' | awk '{print $1}')
-            sed -i "s/^default=.*/default=$number/g" /boot/grub/grub.conf
+    if check_virt kvm; then
+        start
+        if check_centos_main_version 6; then
+            echo -e "[${green}INFO${plain}] System OS is CentOS6. Processing..."
             echo -e "-------------------------------------------"
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 3s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 2s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 1s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Reboot..."
-            reboot
-        else
-            echo -e "[${red}ERROR${plain}] Change kernel failed!"
-        fi
-    elif check_centos_main_version 7; then
-        echo -e "[${green}INFO${plain}] System OS is CentOS7. Processing..."
-        echo -e "-------------------------------------------"
-        rpm -ivh https://filedown.me/Linux/Kernel/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
-        if [ $? -eq 0 ]; then
-            grub2-set-default `awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | grep '(3.10.0-229.1.2.el7.x86_64) 7 (Core)' | awk '{print $1}'`
+            rpm -ivh https://filedown.me/Linux/Kernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
+            rpm -ivh https://filedown.me/Linux/Kernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
+            if [ $? -eq 0 ]; then
+                number=$(cat /boot/grub/grub.conf | awk '$1=="title" {print i++ " : " $NF}' | grep '2.6.32-504' | awk '{print $1}')
+                sed -i "s/^default=.*/default=$number/g" /boot/grub/grub.conf
+                echo -e "-------------------------------------------"
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 3s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 2s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 1s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Reboot..."
+                reboot
+            else
+                echo -e "[${red}ERROR${plain}] Change kernel failed!"
+                exit 1
+            fi
+        elif check_centos_main_version 7; then
+            echo -e "[${green}INFO${plain}] System OS is CentOS7. Processing..."
             echo -e "-------------------------------------------"
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 3s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 2s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Success! Your server will reboot in 1s..."
-            sleep 1
-            echo -e "[${green}INFO${plain}] Reboot..."
-            reboot
-        else
-            echo -e "[${red}ERROR${plain}] Change kernel failed!"
+            rpm -ivh https://filedown.me/Linux/Kernel/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
+            if [ $? -eq 0 ]; then
+                grub2-set-default `awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | grep '(3.10.0-229.1.2.el7.x86_64) 7 (Core)' | awk '{print $1}'`
+                echo -e "-------------------------------------------"
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 3s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 2s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Success! Your server will reboot in 1s..."
+                sleep 1
+                echo -e "[${green}INFO${plain}] Reboot..."
+                reboot
+            else
+                echo -e "[${red}ERROR${plain}] Change kernel failed!"
+                exit 1
+            fi
+        elif check_centos_main_version 8; then
+            echo -e "[${yellow}WARNNING${plain}] This script only support CentOS6/7!"
+            exit 1
         fi
+    else
+        echo -e "[${yellow}WARNNING${plain}] This script only support KVM!"
+        exit 1
     fi
 else
-    echo -e "[${yellow}WARNNING${plain}] This script only support CentOS6/7!"
-	exit 1
+    echo -e "[${yellow}WARNNING${plain}] This script only support CentOS!"
+    exit 1
 fi
